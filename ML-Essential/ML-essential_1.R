6bcdad53922a5efac1693fe433b06bf7ab1fc621
giftkms <giftkms@gmail.com>
2023-08-30 17:30:17 +0700

    model

--- ML-Essential/ML-essential_1.R
+++ ML-Essential/ML-essential_1.R
@@ -26,15 +26,44 @@ clean_df <- full_df %>%
   drop_na()
 
 # 1. split data
-set.seed(42)
+
 
 train_test_split <- function(data){
+  set.seed(42)
   n <- nrow(data)
   train_id <- sample(1:n, size = 0.8*n)
   train_df <- data[train_id,]
   test_df <- data[-train_id,]
-  return(list(train_df, test_df))
+  return(list(training = train_df, 
+              testing = test_df))
 }
+
+prep_data <-train_test_split(clean_df)
+
+train_df <- prep_data[[1]]
+test_df <- prep_data[[2]]
+
+# 2. Train model
+set.seed(42)
+lm_model <- train(mpg ~ . 
+                  ,data = train_df
+                  # algorithm
+                  ,method = "ranger" )
+
+lm_model
+
+# 3. score model
+
+p <- predict(lm_model, newdata = test_df)
+
+
+# 4. evaluate model
+
+(MAE <- mean(abs(p - test_df$mpg)))
+
+(RMSE <- sqrt(mean((p - test_df$mpg)**2)))
+
+
 
 
 
